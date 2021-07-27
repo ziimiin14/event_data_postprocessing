95a716753bb2028240e1203de8ece7f3a919d6a9
ziimiin14 <erzimin@gmail.com>
2021-07-27 16:27:39 +0800

    updated boxblur function

--- boxBlur.py
+++ boxBlur.py
@@ -15,3 +15,46 @@ def boxesForGauss(sigma,n):
     sizes = [wl if i < m else wu for i in range(n)]
 
     return sizes
+
+def gaussBlur_3(scl,tcl,w,h,r):
+    bxs = boxesForGauss(r,3)
+    boxBlur_3(scl,tcl,w,h,int((bxs[0]-1)/2))
+    boxBlur_3(scl,tcl,w,h,int((bxs[1]-1)/2))
+    boxBlur_3(scl,tcl,w,h,int((bxs[2]-1)/2))
+
+def boxBlur_3(scl,tcl,w,h,r):
+    for i in range(len(scl)):
+        
+        tcl[i] = scl[i]
+        # if i <=2:
+        #     print('scl:',scl)
+        #     print('tcl:',tcl)
+        boxBlurH_3(tcl,scl,w,h,r)
+        # if i <=2:
+        #     print('scl1:',scl)
+        #     print('tcl1:',tcl)
+        boxBlurT_3(scl,tcl,w,h,r)
+
+
+def boxBlurH_3(scl,tcl,w,h,r):
+    for i in range(h):
+        for j in range(w):
+            val = 0
+            for ix in range(j-r,j+r+1,1):
+                x = min(w-1,max(0,ix))
+                val += scl[i*w+x]
+
+            tcl[i*w+j] = val/(r+r+1)
+
+def boxBlurT_3(scl,tcl,w,h,r):
+    for i in range(h):
+        for j in range(w):
+            val = 0
+            for iy in range(i-r,i+r+1,1):
+                y = min(h-1,max(0,iy))
+                val += scl[y*w+j]
+
+            tcl[i*w+j] = val/(r+r+1)
+
+
+
